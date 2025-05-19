02b630dec0aebd1959a6f20c4e8a15d9f55eaf15
anthonytilkin <anthonytilkin28@gmail.com>
2025-05-19 14:30:06 -0500

    lobster update

--- Lobster.py
+++ Lobster.py
@@ -18,6 +18,8 @@ def get_price_raw_lobster():
         print(f"Buy Price: {buy_price_raw_lobster} coins")
         print(f"Sell Price: {sell_price_raw_lobster} coins")
 
+        return buy_price_raw_lobster, sell_price_raw_lobster
+    
 def get_price_lobster():
 
     id_lobster = 379
@@ -27,10 +29,26 @@ def get_price_lobster():
         buy_price_lobster = data_lobster.get('high')
         sell_price_lobster = data_lobster.get('low')
 
-        print("Lobster Prices:")
+        print("\nLobster Prices:")
         print(f"Buy Price: {buy_price_lobster} coins")
         print(f"Sell Price: {sell_price_lobster} coins")
 
+        return buy_price_lobster, sell_price_lobster
+    
+def get_profit_lobster(buy_raw, buy_cooked, sell_raw, sell_cooked):
+    
+    buy_buy_profit = buy_cooked - buy_raw
+    buy_sell_profit = buy_cooked - sell_raw
+    sell_buy_profit = sell_cooked - buy_raw
+    sell_sell_profit = sell_cooked - sell_raw
 
-get_price_raw_lobster()
-get_price_lobster()
+    print("\nProfits Per Lobster:")
+    print(f"Buy->Buy Profit: {buy_buy_profit} coins")
+    print(f"Buy->Sell Profit: {buy_sell_profit} coins")
+    print(f"Sell->Buy Profit: {sell_buy_profit} coins")
+    print(f"Sell->Sell Profit: {sell_sell_profit} coins")
+
+buy_raw, sell_raw = get_price_raw_lobster()
+buy_cooked, sell_cooked = get_price_lobster()
+
+get_profit_lobster(buy_raw, buy_cooked, sell_raw, sell_cooked)
